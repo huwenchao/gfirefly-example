99b4ab6c63c9b0c78b6757f408b22bc6165ea5ba
Hu Wenchao <zjuhwc@gmail.com>
2015-11-17 10:05:18 +0800

    format and add game server 2

--- startmaster.py
+++ startmaster.py
@@ -1,9 +1,7 @@
-#coding:utf8
+# coding:utf8
 
-if __name__=="__main__":
+if __name__ == "__main__":
     from gfirefly.master.master import Master
     master = Master()
-    master.config('config.json','appmain.py')
+    master.config('config.json', 'appmain.py')
     master.start()
-    
-    
